193c1ed80fb27de8c899fb27fa382470739cec8f
Zachary Traczyk <cookies951@gmail.com>
2023-01-10 01:15:39 -0800

    increase event info font weight

--- src/views/Resources/EventInfo.tsx
+++ src/views/Resources/EventInfo.tsx
@@ -6,25 +6,23 @@ const EventInfo: React.FC = () => {
     <Card introTitle='Need Something?' title='Event Info'>
       <div className='flex flex-col gap-5 md:gap-10 md:px-20'>
         <div className='flex flex-col gap-2'>
-          <p>WiFi</p>
-          <p className='font-thin'>Use: UCSC_Guest</p>
-          <p className='font-thin'>Pass: XSDSDVFB</p>
+          <p className='font-bold'>WiFi</p>
+          <p>Use: UCSC_Guest</p>
+          <p>Pass: XSDSDVFB</p>
         </div>
 
         <div className='flex flex-col gap-2'>
-          <p>Color Legend</p>
-          <p className='font-thin'>Organizers: Green Shirt/Green Jackets</p>
-          <p className='font-thin'>Mentors/Volunteers: Blue Shirt</p>
-          <p className='font-thin'>Judges: Pink Shirt</p>
+          <p className='font-bold'>Color Legend</p>
+          <p>Organizers: Green Shirt/Green Jackets</p>
+          <p>Mentors/Volunteers: Blue Shirt</p>
+          <p>Judges: Pink Shirt</p>
         </div>
 
         <div className='flex flex-col gap-2'>
-          <p>Slack</p>
-          <p className='font-thin'>
-            Slack will be used for communication during the event.
-          </p>
-          <p className='font-thin'>Mentors/Volunteers: Blue Shirt</p>
-          <p className='font-thin'>
+          <p className='font-bold'>Slack</p>
+          <p>Slack will be used for communication during the event.</p>
+          <p>Mentors/Volunteers: Blue Shirt</p>
+          <p>
             Join our Slack{" "}
             <a className='text-blue italic underline' href='/'>
               here
@@ -34,18 +32,19 @@ const EventInfo: React.FC = () => {
         </div>
 
         <div>
-          <p>Emergency Contacts</p>
+          <p className='font-bold'>Emergency Contacts</p>
           <div className='mt-5 flex flex-col gap-5 md:px-5'>
             <div className='flex flex-col gap-2'>
-              <p>UC Police Dispatch</p>
-              <p className='font-thin'>831-459-2231 ext 1</p>
+              <p className='font-bold'>UC Police Dispatch</p>
+              <p>831-459-2231 ext 1</p>
             </div>
             <div className='flex flex-col gap-2'>
-              <p>Santa Cruz Fire Department</p>
-              <p className='font-thin'>831-420-5678</p>
+              <p className='font-bold'>Santa Cruz Fire Department</p>
+              <p>831-420-5678</p>
             </div>
           </div>
         </div>
+        <div className='h-5'></div>
       </div>
     </Card>
   )
