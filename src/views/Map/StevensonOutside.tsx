5eb1f34ceb418e42a5d9adba012461627ab4a28f
Zachary Traczyk <cookies951@gmail.com>
2023-01-09 20:04:57 -0800

    fix mobile general map

--- src/views/Map/StevensonOutside.tsx
+++ src/views/Map/StevensonOutside.tsx
@@ -11,7 +11,7 @@ interface MapKeyItemInterface {
 const MapKeyItem: React.FC<MapKeyItemInterface> = ({ color, label }) => {
   return (
     <div className='flex items-start gap-5'>
-      <div className={`mt-1 h-6 w-6 shrink-0 rounded-md ${color}`}></div>
+      <div className={`mt-1 h-2 md:h-6 w-6 shrink-0 rounded-md ${color}`}></div>
       <p>{label}</p>
     </div>
   )
@@ -20,13 +20,9 @@ const MapKeyItem: React.FC<MapKeyItemInterface> = ({ color, label }) => {
 const StevensonOutside: React.FC = () => {
   return (
     <Card override='p-5 md:p-5 lg:w-[70rem] lg:ml-[-12rem]'>
-      <div className='flex flex-col gap-10 md:flex-row'>
-        <img
-          className='md:w-2/3'
-          src={StevensonMapImg}
-          alt='UCSC Stevenson College Map'
-        />
-        <div className='h-100 grow-1 flex flex-col justify-evenly gap-5'>
+      <div className='flex flex-col gap-4 md:gap-10 md:flex-row'>
+        <img className='md:w-2/3' src={StevensonMapImg} alt='UCSC Stevenson College Map' />
+        <div className='h-100 grow-1 flex flex-col justify-evenly gap-2 md:gap-5'>
           <MapKeyItem color='bg-[#FF1f22]' label='Stevenson Event Center' />
           <MapKeyItem color='bg-[#FF33EB]' label='Senior Commons Room' />
           <MapKeyItem color='bg-[#FFB800]' label='Wagstaff Fireside Lounge' />
