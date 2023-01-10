452f71b344265f60ca655098a1fcf45d92b0a81d
Zachary Traczyk <cookies951@gmail.com>
2023-01-09 18:08:01 -0800

    add Starter Pack Link

--- src/views/Resources/Hackerpack.tsx
+++ src/views/Resources/Hackerpack.tsx
@@ -4,26 +4,17 @@ import Card from "../../components/Card"
 
 const Hackerpack: React.FC = () => {
   return (
-    <Card
-      introTitle='Ready to Start Hacking?'
-      title='Hackerpack & Starter Pack'
-    >
+    <Card introTitle='Ready to Start Hacking?' title='Hackerpack & Starter Pack'>
       <div className='flex flex-col items-center gap-3'>
         <p className='mb-10 text-left md:w-5/6'>
-          You can find everything you need to know in our Hacker Packet PDF. It
-          covers what you need to bring, travel info, workshop info, and more.
-          Our starter pack will have code that can help you jumpstart your
-          projects!
+          You can find everything you need to know in our Hacker Packet PDF. It covers what you need to bring,
+          travel info, workshop info, and more. Our starter pack will have code that can help you jumpstart
+          your projects!
         </p>
-        <Button
-          text='View Hacker Packet'
-          link=''
-          type='full'
-          override='w-full md:w-1/2'
-        />
+        <Button text='View Hacker Packet' link='' type='full' override='w-full md:w-1/2' />
         <Button
           text='Starter Pack'
-          link=''
+          link='https://github.com/CruzHacks/Cruzhacks-Hacker-Packs'
           type='clear'
           override='w-full md:w-1/2'
         />
