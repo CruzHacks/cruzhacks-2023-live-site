797e075afa822b45dc7cc71a20db8c6076d8b5c2
Zachary Traczyk <me@zacktraczyk.com>
2023-01-29 13:10:34 -0800

    fix firebase config

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -8,38 +8,3 @@ const firebaseConfig = JSON.parse(VITE_FIREBASE_CONFIG)
 
 export const app = initializeApp(firebaseConfig)
 const messaging = getMessaging(app)
-
-export const requestPermission = () => {
-  console.log("Requesting permission...")
-  Notification.requestPermission().then(permission => {
-    if (permission === "granted") {
-      console.log("Notification permission granted.")
-    }
-  })
-}
-
-export const getTokenWrapper = async (
-  setNotificationEnabled: React.Dispatch<React.SetStateAction<boolean>>
-) => {
-  return getToken(messaging, { vapidKey: VITE_FIREBASE_VAPID_KEY })
-    .then(currentToken => {
-      if (currentToken) {
-        setNotificationEnabled(true)
-      } else {
-        setNotificationEnabled(false)
-      }
-      return currentToken
-    })
-    .catch(err => {
-      console.log(err)
-    })
-}
-
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.onBackgroundMessage` handler.
-onMessage(messaging, payload => {
-  console.log("Message received. ", payload)
-  // ...
-})
